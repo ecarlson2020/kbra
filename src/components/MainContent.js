bd604f13eefd5c24df86c8008d81e32356d2f70f
Evan Carlson <ecarlson2020@gmail.com>
2021-12-24 14:50:16 -0700

    fetch actual content

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,9 +2,50 @@
 import React from 'react'
 
 export default class MainContent extends React.Component{
+	state = {
+		fetch: null,
+	}
+
+	fetch_json(){
+		fetch('https://randomuser.me/api/?page=1&results=10&seed=abc')
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+
+				this.setState({
+					fetch: data
+				})
+			})
+		;
+	}
+
+	componentDidMount(){
+		this.fetch_json();
+	}
+
+	map_results(){
+		var fetch = this.state.fetch;
+
+		if(fetch){
+			return fetch['results'].map(result => (
+				<div key={result.id.value}>
+					<div>{result.name.title}</div>
+					<div>{result.name.first}</div>
+					<div>{result.name.last}</div>
+					<div>{result.email}</div>
+					<img src={result.picture.large} />
+				</div>
+			));
+		}
+
+		return "";
+	}
+
 	render(){
 		return (
-			<div>MainContent</div>
+			<div>
+				{this.map_results()}
+			</div>
 		);
 	}
 }
