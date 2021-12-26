ae87e51eaa58f224368efd06ba9da2a43dd13317
Evan Carlson <ecarlson2020@gmail.com>
2021-12-26 02:05:37 -0700

    top menu progress

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -5,7 +5,12 @@ export default class TopMenu extends React.Component{
 	render(){
 		return (
 			<div id='top-menu'>
-				<div>Evan Carlson Technical Challenge</div>
+				<div id='top-menu-inner'>
+					<div className='margin03 font03'>
+						<img src='/logo512.png' />
+						<span>Evan Carlson - Technical Challenge</span>
+					</div>
+				</div>
 			</div>
 		);
 	}
