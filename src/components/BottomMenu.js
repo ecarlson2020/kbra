3f25eb3a53e5d53976456a8e123996b7021661e9
Evan Carlson <ecarlson2020@gmail.com>
2021-12-26 02:59:11 -0700

    starting pagination

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -8,14 +8,27 @@ export default class BottomMenu extends React.Component{
 
 		this.state = {
 			displayed: true,
+			active_page: 3,
 		}
 
 		react_objs.top_menu = this;
 	}
 
+	draw_page_numbers(){
+		var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+		return arr.map((el, i) => (
+			<div key={i} className={'page-number text-center' + (this.state.active_page == el ? " active" : "")}>{el}</div>
+		))
+	}
+
 	render(){
 		return (
-			<div>BottomMenu</div>
+			<div id='bottom-menu' className={'font01 color01 border-radius' + (this.state.displayed ? " transform-none" : "")}>
+				<div className='flex-center'>
+					{this.draw_page_numbers()}
+				</div>
+			</div>
 		);
 	}
 }
